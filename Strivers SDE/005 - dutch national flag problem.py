bd99d022c8687842b0ebf04d617296960bbf5c5f
Gagandeep <gagandeep.singh@cashflo.io>
2023-11-11 23:41:14 +0530

    Minor updates DNF.py

--- Strivers SDE/005 - dutch national flag problem.py
+++ Strivers SDE/005 - dutch national flag problem.py
@@ -4,19 +4,22 @@
     Space Complexity: O(1)
 '''
 def dutch_national_flag_problem(arr):
-    low = 0
-    mid = 0
-    high = len(arr) - 1
-    while mid <= high:
-        if arr[mid] == 0:
-            arr[low], arr[mid]  = arr[mid], arr[low]
-            low += 1
+    low = 0 # low is the index of the first 1
+    mid = 0 # mid is the current element
+    high = len(arr) - 1 
+    while mid <= high: # mid is the index of the last 1
+        if arr[mid] == 0: 
+            # swap arr[low] and arr[mid]
+            arr[low], arr[mid]  = arr[mid], arr[low] # swap arr[low] and arr[mid]
+            low += 1 # 
             mid += 1
-        elif arr[mid] == 1:
+        elif arr[mid] == 1: 
+            # do nothing
             mid += 1
         else:
-            arr[mid], arr[high] = arr[high], arr[mid]
-            high -= 1
+            # swap arr[mid] and arr[high]
+            arr[mid], arr[high] = arr[high], arr[mid] 
+            high -= 1 
     return arr
 
 if __name__ == '__main__':
